8921974bcd07972b0c6a4534f7828ba75f3d14c7
Hunter Hudson <hunterhudson3@gmail.com>
2020-11-09 15:16:21 -0700

    Replace AR cube with sphere (moon object), add dice model, add horizontal plane detection functionality.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,19 +22,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        /*Creates new SCNBox (cube) predefined AR object (NOTE: chamferRadius is how rounded the edges are, units are in METERS, so watch your measurements):
+        /*While this line will eventually be commented out, it is good during testing because it allows the developer to see things (like geometric guides for example) that normally aren't visible to the end user, but are helpful during testing:
         */
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
+        
+        /*Creates new SCNBox (cube) predefined AR object (NOTE: chamferRadius is how rounded the edges are, units are in METERS, so watch your measurements):
+         
         let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        */
         
         //Creates a new material that can be applied to AR objects:
         let material = SCNMaterial()
         
-        //Sets the diffuse (base) color of the material to red:
+        /*Sets the diffuse (base) color of the material to red:
         material.diffuse.contents = UIColor.red
+        */
+        
+        //Sets the contents of the material to the moon image asset using its filepath:
+        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
         
         /*Sets the cube's materials ot the material made above (NOTE: AR materials are arrays):
-        */
+        
         cube.materials = [material]
+        */
         
         //Creates a new node (position in 3D space):
         let node = SCNNode()
@@ -43,8 +53,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         */
         node.position = SCNVector3(x: 0, y: 0.1, z: -1.5)
         
-        //Sets the node's geometry as the previously created cube.
+        /*Sets the node's geometry as the previously created cube.
         node.geometry = cube
+        */
         
         //This line adds a "child node" to the "root note" of the sceneView:
         sceneView.scene.rootNode.addChildNode(node)
@@ -52,6 +63,31 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         /*This line allows the sceneView to use lighting from user's surroundings instead of just the scn graph:
         */
         sceneView.autoenablesDefaultLighting = true
+        
+        /*Creates a new Scene object as a sphere this time (NOTE: measurements are still in METERS):
+        */
+        let sphere = SCNSphere(radius: 0.2)
+        
+        //Assigns the material made above to the sphere:
+        sphere.materials = [material]
+        
+        //Sets the node's geometry to the sphere made above:
+        node.geometry = sphere
+        
+        //Initializes a new scene with the filepath of the dice object:
+        let diceScene = SCNScene(named: "art.scnassets/diceeColada copy.scn")
+        
+        /*Initializes the node of the dice object to the node of the dice object in the scene tree (NOTE: "recursively" parameter is a bool for whether or not you wnat to include every single other object in the tree for the scene):
+        */
+        let diceNode = diceScene?.rootNode.childNode(withName: "Dice", recursively: true)
+        
+        //Sets the diceNode's position:
+        if let safeDiceNode = diceNode{
+            safeDiceNode.position = SCNVector3(x: 0, y: 0, z: 0.1)
+            
+            //Adds the diceNode to the rootNode of the sceneView:
+            sceneView.scene.rootNode.addChildNode(safeDiceNode)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -62,6 +98,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             /* Create a session configuration; ARWorldTrackingConfiguration is better because it actually tracks the world around you, but doesn't work if the device doesn't at least have the A9 chip:
             */
             let configuration = ARWorldTrackingConfiguration()
+            
+            //Sets the configuration of the sceneView to recognize horizontal planes:
+            configuration.planeDetection = .horizontal
             
             // Run the view's session
             sceneView.session.run(configuration)
@@ -79,6 +118,63 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
+    }
+    
+    /*This method triggers when the user touches a location on the screen. The location is then converted into an AR location in the world:
+    */
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        /*If we were using multitouch, we wouldn't use first here, but since we're only worried about one touch for this example, "touches.first" (which is the first detected touch in an array of detected touches) is optionally bound here:
+        */
+        if let touch = touches.first {
+            
+        }
+    }
+    
+    /*This method is called when a node is added to the sceneView (NOTE: the "ARAnchor" is a position on a horizontal plane on which an AR object can be placed):
+    */
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        //Checks if anchor is of type ARPlaneAnchor (this block prevents runtime errors):
+        if anchor is ARPlaneAnchor {
+            print("Plane detected.")
+            
+            /*If anchor is of type ARPlaneAnchor, downcasts planeAnchor constant as ARPlaneAnchor:
+            */
+            let planeAnchor = anchor as! ARPlaneAnchor
+            
+            /*Initializes a new constant plane equal to a SCNPlane at the extent of planeAnchor at x and z positions (NOTE: do not use y for the second parameter, as height refers to the distance from the user):
+            */
+            let plane = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+            
+            //Initializes planeNode as a SCNNode object:
+            let planeNode = SCNNode()
+            
+            /*Sets the position of the planeNode as the center of the planeAnchor. REMEMBER: y is typically unmodified because if it isn't, the object will wind up above (or worse, below) the horizontal plane, which would be pointless:
+            */
+            planeNode.position = SCNVector3(x: planeAnchor.center.x, y: 0, z: planeAnchor.center.z)
+            
+            /*SCNNodes are vertical by default, so a transformation is necessary to make planeNode horizontal. Since we're rotating using the x-axis (vertical to horizontal), the x axis is the only parameter that is not equal to zero (NOTE: parameters for this rotation are in RADIANS, not DEGREES. Also, can use "Float.pi" predefined property in order to work with pi. Float.pi is negative since we're rotating counterclockwise in order to make the vertical plane horizontal):
+            */
+            planeNode.transform = SCNMatrix4MakeRotation((-Float.pi / 2), 1, 0, 0)
+            
+            //Creates a new material for the grid of the plane:
+            let gridMaterial = SCNMaterial()
+            
+            //Sets the gridMaterial's contents to an image with the grid's filepath:
+            gridMaterial.diffuse.contents = UIImage(named: "art.scnassets/grid.png")
+            
+            //Adds gridMaterial to plane:
+            plane.materials = [gridMaterial]
+            
+            //Sets the geometry of the planeNode to the plane object:
+            planeNode.geometry = plane
+            
+            /*Uses the inputted node parameter of the method to insert the planeNode as a child node:
+            */
+            node.addChildNode(planeNode)
+        } else {
+            //Exits the method if the plane isn't detected (this prevents runtime errors).
+            return
+        }
     }
 
     // MARK: - ARSCNViewDelegate
