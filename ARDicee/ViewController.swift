9fe2bbcf4c0c4709f231a9a9e1c9c11464b27ded
Hunter Hudson <hunterhudson3@gmail.com>
2020-11-09 13:15:30 -0700

    Add check to see if device is supported, add support for world tracking-incompatible iOS devices, create cube AR object.

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -22,21 +22,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        /*Creates new SCNBox (cube) predefined AR object (NOTE: chamferRadius is how rounded the edges are, units are in METERS, so watch your measurements):
+        */
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        //Creates a new material that can be applied to AR objects:
+        let material = SCNMaterial()
+        
+        //Sets the diffuse (base) color of the material to red:
+        material.diffuse.contents = UIColor.red
+        
+        /*Sets the cube's materials ot the material made above (NOTE: AR materials are arrays):
+        */
+        cube.materials = [material]
+        
+        //Creates a new node (position in 3D space):
+        let node = SCNNode()
+        
+        /*Sets the node's position (NOTE: x-axis position: negative to the left, positive to the right, y-axis position: negative towards the ground, positive towards the ceiling, z-axis position: POSITIVE AWAY FROM USER, NEGATIVE TOWARDS USER):
+        */
+        node.position = SCNVector3(x: 0, y: 0.1, z: -1.5)
+        
+        //Sets the node's geometry as the previously created cube.
+        node.geometry = cube
+        
+        //This line adds a "child node" to the "root note" of the sceneView:
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        /*This line allows the sceneView to use lighting from user's surroundings instead of just the scn graph:
+        */
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
+        //Checks if ARWorldTrackingConfiguraiton is supported:
+        if (ARWorldTrackingConfiguration.isSupported) {
+            /* Create a session configuration; ARWorldTrackingConfiguration is better because it actually tracks the world around you, but doesn't work if the device doesn't at least have the A9 chip:
+            */
+            let configuration = ARWorldTrackingConfiguration()
+            
+            // Run the view's session
+            sceneView.session.run(configuration)
+        } else {
+            //Runs the AR configuration without world tracking if it's not:
+            let configuration = AROrientationTrackingConfiguration()
+            
+            // Run the view's session
+            sceneView.session.run(configuration)
+        }
     }
     
     override func viewWillDisappear(_ animated: Bool) {
